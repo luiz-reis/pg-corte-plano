22d6ac3c8267643ccf609e8772e4a29370c94624
Luiz Reis <luzrei@hotmail.com>
2016-06-16 14:55:36 -0300

    close #1 implementado classe Vetor

---
+++ Vetor.cpp
@@ -0,0 +1,94 @@
+#include "Vetor.h"
+#include <math.h>
+#include <iostream>
+
+Vetor::Vetor()
+{
+	this->x = this->y = this->z = 0;
+}
+
+Vetor::Vetor(float x, float y, float z)
+{
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
+
+float Vetor::norma() const
+{
+	return sqrt(this->x * this->x + this->y * this->y);
+}
+
+void Vetor::m_escalar(float escalar)
+{
+	this->x *= escalar;
+	this->y *= escalar;
+	this->z *= escalar;
+}
+
+void Vetor::normalizar()
+{
+	m_escalar(1/norma());
+}
+
+Vetor Vetor::m_escalar(const Vetor& vetor, const float escalar)
+{
+	Vetor ret = vetor;
+	ret.m_escalar(escalar);
+	
+	return ret;
+}
+
+Vetor Vetor::normalizar(const Vetor& vetor)
+{
+	Vetor ret = vetor;
+	ret.normalizar();
+	
+	return ret;
+}
+
+float Vetor::p_escalar(const Vetor& v1, const Vetor& v2)
+{
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+Vetor& Vetor::operator+=(const Vetor& rhs)
+{
+	this->x += rhs.x;
+	this->y += rhs.y;
+	this->z += rhs.z;
+	
+	return *this;
+}
+
+Vetor operator+(Vetor lhs, const Vetor& rhs)
+{
+	lhs += rhs;
+	return lhs;
+}
+
+Vetor& Vetor::operator*=(const Vetor& rhs)
+{
+//	a × b = [a2b3 − a3b2, a3b1 − a1b3, a1b2 − a2b1].
+	float x = this->y * rhs.z - this->z * rhs.y;
+	float y = this->z * rhs.x - this->x * rhs.z;
+	float z = this->x * rhs.y - this->y * rhs.x;
+	
+	this->x = x;
+	this->y = y;
+	this->z = z;
+	
+	return *this;
+}
+
+Vetor operator*(Vetor lhs, const Vetor& rhs)
+{
+	lhs *= rhs;
+	return lhs;
+}
+
+std::ostream& operator<< (std::ostream& out, const Vetor& vetor)
+{
+	out << "{ " << vetor.x << ", " << vetor.y << ", " << vetor.z << " }";
+	return out;
+}
--- vetor.h
+++ vetor.h
@@ -1,10 +1,30 @@
 #ifndef VETOR_H
 #define VETOR_H
 
+#include <iostream>
+
 class Vetor
 {
+public:
 	float x, y, z;
-    Ponto(float x, float y, float z) : x(x), y(y), z(z) {};
+	Vetor();
+	Vetor(float x, float y, float z);
+	
+	float norma() const;
+	void m_escalar(const float escalar);
+	void normalizar();
+	
+	static float p_escalar(const Vetor& v1, const Vetor& v2);
+	static Vetor m_escalar(const Vetor& vetor, const float escalar);
+	static Vetor normalizar(const Vetor& vetor);
+	
+	friend Vetor operator+(Vetor lhs, const Vetor& rhs);
+	Vetor& operator+=(const Vetor& rhs);
+	
+	friend Vetor operator*(Vetor lhs, const Vetor& rhs); // produto vetorial
+	Vetor& operator*=(const Vetor& rhs);
+	
+	friend std::ostream& operator<< (std::ostream& out, const Vetor& vetor);
 };
 
 #endif
