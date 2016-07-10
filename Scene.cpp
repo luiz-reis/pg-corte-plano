4bcaa76af8d6889a8a692e3d55a274f6f8d5bac3
rcalegario <rodrigocalegario@gmail.com>
2016-07-09 21:09:04 -0300

    criacao do metodo de iluminacao

--- Scene.cpp
+++ Scene.cpp
@@ -1,6 +1,7 @@
 #include "Scene.h"
 #include <algorithm>
 #include <iostream>
+#include <cmath>
 
 Scene::Scene() 
 	: Scene(nullptr)
@@ -143,6 +144,43 @@ void Scene::draw()
 	}
 }
 
+Color Scene::lumination(Vetor point, Vetor normal, Material material)
+{
+	if(Vetor::p_escalar(normal, point) < 0){
+		normal = Vetor::m_escalar(normal, -1);
+	}
+
+	int r = Util::clamp(material.get_ka()*ia.r, 0, 255);
+	int g = Util::clamp(material.get_ka()*ia.g, 0, 255);
+	int b = Util::clamp(material.get_ka()*ia.b, 0, 255);
+	Color parte1 = new Color(r_parte1, g_parte1, b_parte1);
+
+	Vetor L = lights[0].pos - point;
+	float nl = Vetor::p_escalar(normal, L)
+	
+	if(Vetor::p_escalar(normal, L) >= 0){
+		float parte2 = material.get_kd() * nl;
+		int r_parte2 = Util::clamp(material.get_od().r*lights[0].color.r, 0, 255);
+		int g_parte2 = Util::clamp(material.get_od().g*lights[0].color.g, 0, 255);
+		int b_parte2 = Util::clamp(material.get_od().b*lights[0].color.b, 0, 255);
+		int r += Util::clamp(r_parte2, 0, 255);
+		int g += Util::clamp(g_parte2, 0, 255);
+		int b += Util::clamp(b_parte2, 0, 255);
+
+		Vetor 2n = Vetor::m_escalar(normal, 2.0f);
+		Vetor R = Vetor::m_escalar(2n, nl) - L;
+		if(Vetor::p_escalar(point, R) >= 0){
+			float rv = pow(Vetor::p_escalar(R, point), material.get_n());
+			float parte3 = material.get_ks()*rv;
+			int r += Util::clamp(parte3*lights[0].color.r, 0, 255);
+			int g += Util::clamp(parte3*lights[0].color.g, 0, 255);
+			int b += Util::clamp(parte3*lights[0].color.b, 0, 255);
+		}
+		
+	}
+
+	return Color(r, g, b);
+}
 
 
 
