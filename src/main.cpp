11bca8bc6abbd3bba232feccb9e40b57c66582b4
luiz-reis <lrnn@cin.ufpe.br>
2017-12-22 01:10:38 -0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#ifdef __APPLE__
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+  #include <windows.h>
+#endif
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
 #include <stdlib.h>
 #include "Vetor.h"
 #include "Scene.h"
